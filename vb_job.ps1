b0a646323255e764b96372c478312cfa60e4c4a7
franciscotudel-overtel <74053305+franciscotudel-overtel@users.noreply.github.com>
2020-11-11 17:53:54 +0100

    Primera subida
    Primera subida

---
+++ vb_job.ps1
@@ -0,0 +1,150 @@
+# Script: VB_Job
+# Author: Francisco Tudel
+# Description: Query Veeam job information
+# License:
+#
+# This script is intended for use with PandoraFMS
+#
+#
+# USAGE:
+#   as a script: C:\WINDOWS\system32\windowspowershell\v1.0\powershell.exe -command  "& C:\path\vb_job.ps1 <ITEM_TO_QUERY> <JOBID>"
+#
+#
+$version = "0.1"
+
+$ITEM = [string]$args[0]
+$ID = [string]$args[1]
+
+#* Load Veeam snapin
+Add-PsSnapin -Name VeeamPSSnapIn -ErrorAction SilentlyContinue
+
+# Query VEEAM for Job
+
+# Open JSON object
+$output =  ""
+
+# ID de prueba
+# $ID = "f2f89dfb-c111-40a1-bc7f-8b7e9d7a72ff"
+# $query2 = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
+# $query3 = Get-VBRBackup | Where-Object {$_.Id -like "*$ID*"}
+#$query = Get-VBRBackupSession | Where {$_.jobId -eq $query2.Id.Guid} | Sort EndTimeUTC -Descending | Select -First 1
+# $query = Get-VBRBackupSession | Where {$_.jobId -eq "$ID"} | Sort EndTimeUTC -Descending | Select -First 1
+
+
+#$query2
+switch ($ITEM) {
+    "JobName" {
+	  $query = Get-VBRBackupSession | Where {$_.jobId -eq "$ID"} | Sort EndTimeUTC -Descending | Select -First 1
+	  $output = $output + $query.Info.JobName
+	  $query = ''
+	}
+	"IsEnabled" {
+	  $query = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
+	  $output = $output + [bool]$query.IsScheduleEnabled 
+	  $query = ''
+	}
+	"Result" {
+	  $query = Get-VBRBackupSession | Where {$_.jobId -eq "$ID"} | Sort EndTimeUTC -Descending | Select -First 1
+	  $output = $output + $query.Info.Result 
+	  $query = ''
+	}
+	"IsRunning" {
+	  $query = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
+	  $output = $output + [bool]$query.IsRunning 
+	  $query = ''
+	}
+	"VmCount" {
+	  $query = Get-VBRBackupSession | Where {$_.jobId -eq "$ID"} | Sort EndTimeUTC -Descending | Select -First 1
+	  $output = $output + $query.Info.Progress.TotalObjects
+	  $query = ''
+	}
+	"TargetDir" {
+	  $query = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
+	  $output = $output + $query.TargetDir
+	  $query = ''
+	}
+	"TargetFile" {
+	  $query = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
+	  $output = $output + $query.TargetFile
+	  $query = ''
+	}
+	"JobType" {
+	  $query = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
+	  $output = $output + $query.JobType 
+	  $query = ''
+	}
+	"NextRun" {
+	  $query = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
+	  $output = $output + ([datetime]$query.ScheduleOptions.NextRun).ToString("dd/MM/yyyy HH:mm:ss")
+	  $query = ''
+	}
+	"LastRunStart" {
+	  $query = Get-VBRBackupSession | Where {$_.jobId -eq "$ID"} | Sort EndTimeUTC -Descending | Select -First 1
+	  #$output = $output + $query2.ScheduleOptions.LatestRunLocal.ToString("dd/MM/yyyy HH:mm:ss")
+	  #$output = $output + $query2.ScheduleOptions.LatestRunLocal
+	  $output = $output + ([datetime]$query.Info.Progress.StartTimeLocal).ToString("dd/MM/yyyy HH:mm:ss")
+	  $query = ''
+	}
+	"LastRunStop" {
+	  $query = Get-VBRBackupSession | Where {$_.jobId -eq "$ID"} | Sort EndTimeUTC -Descending | Select -First 1
+	  $output = $output + ([datetime]$query.Info.Progress.StopTimeLocal).ToString("dd/MM/yyyy HH:mm:ss")
+	  $query = ''
+	}
+	"LastRunMinutes" {
+	  $query = Get-VBRBackupSession | Where {$_.jobId -eq "$ID"} | Sort EndTimeUTC -Descending | Select -First 1
+	  $output = $output + [int]$query.Info.Progress.Duration.TotalMinutes
+	  $query = ''
+	}
+	"TransferedSize" {
+	  $query = Get-VBRBackupSession | Where {$_.jobId -eq "$ID"} | Sort EndTimeUTC -Descending | Select -First 1
+	  $output = $output + [long]$query.Info.Progress.TransferedSize
+	  $query = ''
+	}
+	"RetainDays" {
+	  $query2 = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
+	  $output = $output +  [int]$query2.Options.BackupStorageOptions.RetainDays
+	  $query = ''
+	}
+	"RunManually" {
+	  $query = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
+	  $output = $output +  [int]$query.Options.JobOptions.RunManually
+	  $query = ''
+	}
+	"All" {
+	  $query = Get-VBRJob | Where-Object {$_.Id -like "*$ID*"}
+	  $output = $output + $query 
+	  $query = ''
+	}
+	"TimeFromLast" {
+	$query = Get-VBRBackupSession | Where {$_.jobId -eq "$ID"} | Sort EndTimeUTC -Descending | Select -First 1
+	$res = Get-Date
+	$res -= [datetime]$query.Info.Progress.StopTimeLocal
+	#$res.TotalHours
+	#[math]::round($res.TotalHours,1)
+	[int]$res.TotalHours
+	$query = ''
+	}
+	"TimeFromLastMinutes" {
+	$query = Get-VBRBackupSession | Where {$_.jobId -eq "$ID"} | Sort EndTimeUTC -Descending | Select -First 1
+	$res = Get-Date
+	$res -= [datetime]$query.Info.Progress.StopTimeLocal
+	[int]$res.TotalSeconds
+	$query = ''
+    }
+	"Version" {
+	$output = $version
+    }
+	"ListCSV" {
+	$count = $query | Measure-Object
+    $count = $count.count
+	$query = Get-VBRJob
+    foreach ($object in $query) {
+	  $Id = [string]$object.Id
+	  $Name = [string]$object.Name
+	  $Schedule = [int]$object.IsScheduleEnabled
+	  $output = $output + $Name + ";" + $Schedule + ";" + $Id + ";`r`n"
+      }
+	 $query = ''
+    }
+}
+Write-Host $output
---
+++ vb_test.cmd
@@ -0,0 +1,51 @@
+@echo off
+@rem Bck 16
+@set JOB=f2f89dfb-c111-40a1-bc7f-8b7e9d7a72ff
+@echo Este Script se usa para recoger alguna o todas las consultas posibles de un solo trabajo de para ello, 
+@echo  primero recoge la lista de todas las tareas con su UID, de este resultado cogemos alguna de las UID 
+@echo  y la ponemos en la variable JOB de este script.
+@echo -Jobs-
+@echo.
+@rem %SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_jobs.ps1
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 ListCSV
+@echo Tarea consultada: %JOB%
+@echo.
+@echo -JobName-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 JobName %JOB%
+@echo -RetainDays-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 RetainDays %JOB%
+@echo -TargetDir-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 TargetDir %JOB%
+@echo -TargetFile-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 TargetFile %JOB%
+@echo -LastRunStart-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 LastRunStart %JOB%
+@echo -LastRunStop-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 LastRunStop %JOB%
+@echo -LastRunMinutes-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 LastRunMinutes %JOB%
+@echo -NextRun-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 NextRun %JOB%
+@echo -JobType-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 JobType %JOB%
+@echo -All-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 All %JOB%
+@echo -VmCount-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 VmCount %JOB%
+@echo -IsEnabled-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 IsEnabled %JOB%
+@echo -Result-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 Result %JOB%
+@echo -IsRunning-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 IsRunning %JOB%
+@echo -RunManually-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 RunManually %JOB%
+@echo -TransferedSize-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 TransferedSize %JOB%
+@echo -TimeFromLast-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 TimeFromLast %JOB%
+@echo -TimeFromLastMinutes-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 TimeFromLastMinutes %JOB%
+@echo -Version-
+@%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -nologo -command C:\zabbix\scripts_pandorafms\vb_job.ps1 Version %JOB%
+
